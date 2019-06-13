9cd3dad5fb28d9bc14b1a7172f0c2208b865d12b
muath <muathqendel1997@gmail.com>
2019-06-14 00:29:06 +0300

    hi

---
+++ app/src/main/java/com/qendel/fast/Tasker.java
@@ -0,0 +1,13 @@
+package com.qendel.fast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tasker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasker);
+    }
+}
